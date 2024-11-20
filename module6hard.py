c98de8b1f49f4e4e8b51e7552afbb84aeddf0d4b
Aktinidia <nata_kul@mail.ru>
2024-11-20 21:15:09 +0300

    Add files via upload

--- module6hard.py
+++ module6hard.py
@@ -2,7 +2,7 @@ import math
 class Figure:
     sides_count = 0
 
-    def __init__(self, *sides, color, filled=False):
+    def __init__(self, color, *sides, filled=False):
         self.__color = color
         self.__sides = sides
         self.filled = filled
@@ -39,7 +39,53 @@ class Circle(Figure):
     sides_count = 1
 
     def __init__(self, color, length, filled=False):
-        __radius = self.lenght / (2*math.pi)
+        super().__init__(color, length, filled=filled)
+        self.__radius = length / (2*math.pi)
 
     def get_square(self):
-        return math.pi * sqr(__radius)
+        return len(self) ** 2 / (4*math.pi)
+
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def __init__(self, color, heigth, *sides, filled=False):
+        super().__init__(color, *sides, filled=filled)
+        self.__height = height
+
+    def get_square(self):
+        p = (len(self) / 2)
+        sides = self.get_sides()
+        return math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]))
+
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, color, side, filled=False):
+        cube_sides = [side] * 12
+        super().__init__(color, *cube_sides, filled=filled)
+
+    def get_volume(self):
+        return self.get_sides()[0] ** 3
+
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77) # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15) # Изменится
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
